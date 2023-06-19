ed4e77e00daf2cb8b251cf9ef7cd17b73667827c
parunchxi <parunchxi@gmail.com>
2023-06-19 22:46:08 +0700

    Add data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { productData } from "./data/productData";
+
 function App() {
-  return <></>;
+  console.log(productData);
+  return (
+    <>
+      {productData.map((product) => {
+        return (
+          <div key={product.id}>
+            <h1>{product.title}</h1>
+            <p>{product.description}</p>
+            <p>{product.price}</p>
+            <img src={product.images.preview} />
+            <img src={product.images.model[0]} />
+            <img src={product.images.model[1]} />
+            <img src={product.images.model[2]} />
+            <img src={product.images.design} />
+            <hr />
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default App;
