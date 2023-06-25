19b446a7d66eb00b6d06c96b989f79805ed3a2f6
parunchxi <parunchxi@gmail.com>
2023-06-25 16:19:13 +0700

    Update Homepage

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,19 +6,13 @@ const Footer: React.FC = () => {
     <footer className={styles.footer}>
       <div className="container">
         <h1>
-          New Style For You |{" "}
+          New Style |{" "}
           <a href="https://github.com/parunchxi/React-Ecommerce-Website">
             Github
           </a>
         </h1>
       </div>
-      <div className={styles.bg}>
-        <div className={styles.shape_group}>
-          <div className={styles.shape1}></div>
-          <div className={styles.shape2}></div>
-          <div className={styles.shape3}></div>
-        </div>
-      </div>
+      <div className={styles.bg}></div>
     </footer>
   );
 };
